cacdeb84d27303f122bce3f0572a21a11facc54b
StarButNoPatrik <anishadas963@gmail.com>
2024-07-10 11:00:47 +0530

    hello world changes

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,13 @@
 import streamlit as st
-st.title('Hello world')
-st.write("This is a simple Hello world Streamlit application.")
+
+st.title('Sample Streamlit Run..!!')
+
+name = st.text_input("Enter your name:")
+
+if name:
+    if name.isnumeric():
+        st.error("Error: Please enter a valid name.")
+    else:
+        st.write(f"Hello {name}!!")
+else:
+    st.write("Please enter your name.")
