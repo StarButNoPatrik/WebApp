2a4bf4688f09209d4fe4c4460bffbe4a886c04fb
StarButNoPatrik <anishadas963@gmail.com>
2024-07-10 10:40:18 +0530

    sample changes

---
+++ helloWorld.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title('Sample Streamlit Run..!!')
+
+name = st.text_input("Enter your name:")
+
+if name:
+    if name.isnumeric():
+        st.error("Error: Please enter a valid name, not a number.")
+    else:
+        st.write(f"Hello {name}!!")
+else:
+    st.write("Please enter your name.")
---
+++ readme.md
@@ -0,0 +1,4 @@
+## This is sample Streamlit project
+
+
+### Testing branching strategy in Git
