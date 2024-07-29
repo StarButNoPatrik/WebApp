68e2d175903a50951d4c32b9d94182777887eb38
StarButNoPatrik <anishadas963@gmail.com>
2024-07-29 14:26:52 +0530

    test

--- test.py
+++ test.py
@@ -2,14 +2,14 @@ from streamlit.testing.v1 import AppTest
 
 def test_input_name_success():
     """A user input name clicks enter"""
-    at = AppTest.from_file("helloWorld.py")
+    at = AppTest.from_file("helloworld.py")
     at.run()
     at.text_input[0].input("Anisha").run()
     assert "Hello Anisha!!" in "Hello Anisha!!"
 
 def test_input_name_fail():
     """A user input name clicks enter"""
-    at = AppTest.from_file("helloWorld.py")
+    at = AppTest.from_file("helloworld.py")
     at.run()
     at.text_input[0].input("Anisha").run()
     assert "Hello Anisha!!" in "Hello Anisha!!"
